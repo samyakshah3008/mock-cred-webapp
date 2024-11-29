14b2973ceabe668e2fd196d3e4f33480ee7a00d9
Samyak Shah <samyakshah3008@gmail.com>
2024-11-29 21:37:44 +0530

    service functions add for onboarding

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,13 +1,11 @@
-import fs from "fs";
-import { User } from "../models/user.model.js";
 import {
   checkIfOnboardingCompletedOrNotService,
   getUserDetailsService,
+  saveOnboardingDetailsService,
+  saveStepTwoOnboardingDetailsService,
 } from "../services/user.service.js";
 import { ApiError } from "../utils/api-error.js";
-import { ApiResponse } from "../utils/api-response.js";
 import { asyncHandler } from "../utils/async-handler.js";
-import { uploadOnCloudinary } from "../utils/cloudinary.js";
 
 const getUserDetails = asyncHandler(async (req, res) => {
   const user = req?.user;
@@ -58,43 +56,26 @@ const saveOnboardingDetails = asyncHandler(async (req, res) => {
     return res.status(400).json(new ApiError(400, {}, "Missing fields!"));
   }
 
-  const findUser = await User.findById(userId);
-  if (!findUser) {
-    return res.status(400).json(new ApiError(400, {}, "User not found!"));
-  }
-
-  if (stepCount == 1) {
-    const { socialAccountLink, username, howDoUserPlanToUseApp } = detailsObj;
-
-    if (
-      !socialAccountLink?.length ||
-      !username?.length ||
-      !howDoUserPlanToUseApp?.length
-    ) {
-      return res.status(400).json(new ApiError(400, {}, "Missing fields!"));
+  try {
+    const response = await saveOnboardingDetailsService(
+      userId,
+      detailsObj,
+      stepCount
+    );
+    return res.status(200).json(response);
+  } catch (error) {
+    if (error instanceof ApiError) {
+      return res.status(error.statusCode).json(error);
     }
-
-    findUser.onboardingDetails.stepOne.socialAccountLink = socialAccountLink;
-    findUser.onboardingDetails.stepOne.username = username;
-    findUser.onboardingDetails.stepOne.howDoUserPlanToUseApp =
-      howDoUserPlanToUseApp;
-
-    await findUser.save();
-
     return res
-      .status(200)
-      .json(new ApiResponse(200, {}, "Step 1 details saved successfully!"));
-  } else if (stepCount == 3) {
-    const { availability } = detailsObj;
-    findUser.onboardingDetails.stepThree.availability = availability;
-
-    findUser.isOnboardingComplete = true;
-
-    await findUser.save();
-
-    return res
-      .status(200)
-      .json(new ApiResponse(200, {}, "Step 3 details saved successfully!"));
+      .status(500)
+      .json(
+        new ApiError(
+          500,
+          { errorData: error },
+          error?.message || "something went wrong while checking onboarding"
+        )
+      );
   }
 });
 
@@ -114,37 +95,25 @@ const saveStepTwoOnboardingDetails = asyncHandler(async (req, res) => {
         );
     }
 
-    const localFilePath = file.path;
-    const result = await uploadOnCloudinary(localFilePath);
-
-    if (!result) {
-      return res.status(500).json({ message: "Failed to upload image" });
-    }
-
-    fs.unlinkSync(localFilePath);
-
-    const user = await User.findByIdAndUpdate(
-      req?.user?._id,
-      { "onboardingDetails.stepTwo.profilePicURL": result.secure_url },
-      { new: true, runValidators: true }
+    const response = saveStepTwoOnboardingDetailsService(
+      aboutText,
+      file,
+      req?.user?._id
     );
-
-    if (!user) {
-      return res.status(404).json({ message: "User not found" });
-    }
-
-    user.onboardingDetails.stepTwo.aboutText = aboutText;
-
-    await user.save();
-
-    res.status(200).json({
-      message: "Profile picture uploaded successfully",
-      profilePicURL: user.onboardingDetails.stepTwo.profilePicURL,
-    });
+    return res.status(200).json(response);
   } catch (error) {
-    res
+    if (error instanceof ApiError) {
+      return res.status(error.statusCode).json(error);
+    }
+    return res
       .status(500)
-      .json({ message: "Internal Server Error", error: error.message });
+      .json(
+        new ApiError(
+          500,
+          { errorData: error },
+          error?.message || "something went wrong while saving onboarding step2"
+        )
+      );
   }
 });
 
