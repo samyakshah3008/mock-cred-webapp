8a4d9983f3e3fe324909e07c67bb460736e76816
Samyak Shah <samyakshah3008@gmail.com>
2024-11-29 16:46:31 +0530

    auth and user routes wire in root router

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,5 +1,10 @@
 import { Router } from "express";
+import authRouter from "./auth.route.js";
+import userRouter from "./user.route.js";
 
 const router = Router();
+
+router.use("/auth", authRouter);
+router.use("/user", userRouter);
 
 export default router;
