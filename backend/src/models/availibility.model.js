ae9df286a0d4093e2337c3646ebcb81918d48653
Samyak Shah <samyakshah3008@gmail.com>
2024-11-29 16:44:55 +0530

    feat - availibility model add

---
+++ backend/src/models/availibility.model.js
@@ -0,0 +1,23 @@
+import { model, Schema } from "mongoose";
+
+const daySchema = new Schema({
+  isAvailable: { type: Boolean, required: true },
+  startTime: { type: String },
+  endTime: { type: String },
+});
+
+export const availabilitySchema = new Schema({
+  userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  timeGap: { type: Number, required: true, min: 0 },
+  monday: { type: daySchema, required: true },
+  tuesday: { type: daySchema, required: true },
+  wednesday: { type: daySchema, required: true },
+  thursday: { type: daySchema, required: true },
+  friday: { type: daySchema, required: true },
+  saturday: { type: daySchema, required: true },
+  sunday: { type: daySchema, required: true },
+});
+
+const Availability = model("Availability", availabilitySchema);
+
+export default Availability;
