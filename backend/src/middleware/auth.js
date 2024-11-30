5691d3cf9f0a469258b849e220fb2d6203773a0d
Samyak Shah <samyakshah3008@gmail.com>
2024-11-30 23:21:49 +0530

    fix: auth middleware fixes

--- backend/src/middleware/auth.js
+++ backend/src/middleware/auth.js
@@ -1,27 +1,23 @@
-import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
 import { User } from "../models/user.model.js";
 import { ApiError } from "../utils/api-error.js";
 import { asyncHandler } from "../utils/async-handler.js";
 
-dotenv.config({
-  path: ".env",
-});
-
 export const verifyJWT = asyncHandler(async (req, res, next) => {
   try {
     const token =
-      req?.cookies?.accessToken ||
-      req?.header("Authorization")?.replace("Bearer", "")?.trim();
+      req.cookies?.accessToken ||
+      req.header("Authorization")?.replace("Bearer", "")?.trim();
     if (!token) {
-      throw res.status(401).json(new ApiError(401, {}, "Unauthorized request"));
+      throw new ApiError(401, "Unauthorized request");
     }
     const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
     const user = await User.findById(decodedToken?._id).select(
-      "- refreshToken"
+      "-password -refreshToken"
     );
 
     if (!user) {
-      throw res.status(401).json(new ApiError(401, {}, "Invalid access token"));
+      throw new ApiError(401, "Invalid Access Token");
     }
 
     req.user = user;
@@ -29,6 +25,6 @@ export const verifyJWT = asyncHandler(async (req, res, next) => {
   } catch (error) {
     throw res
       .status(401)
-      .json(new ApiError(401, { errorData: error }, "invalid or expired jwt"));
+      .json(new ApiError(401, error, "invalid or expired jwt"));
   }
 });
