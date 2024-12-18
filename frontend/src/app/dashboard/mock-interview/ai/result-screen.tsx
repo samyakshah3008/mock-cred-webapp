0ce94203d2ecbb4009ec8fb76563a620af7ef53f
Samyak Shah <samyakshah3008@gmail.com>
2024-12-18 16:32:41 +0530

    feat - result screen api integrations add to store mock interview details

--- frontend/src/app/dashboard/mock-interview/ai/result-screen.tsx
+++ frontend/src/app/dashboard/mock-interview/ai/result-screen.tsx
@@ -1,6 +1,39 @@
 "use client";
 
-const ResultScreen = ({ userFinalFeedbackObj }: any) => {
+import { useToast } from "@/hooks/use-toast";
+import { addNewEntryToDBService } from "@/services/ai-mock-interview.service";
+import { useEffect } from "react";
+
+const ResultScreen = ({ userFinalFeedbackObj, mockInterviewObj }: any) => {
+  const { toast } = useToast();
+
+  const addNewEntryToDB = async () => {
+    const payload = {
+      jobDescription: mockInterviewObj?.jobDescription,
+      jobRole: mockInterviewObj?.jobRole,
+      yearsOfExperience: mockInterviewObj?.yearsOfExperience,
+      interviewDetails: userFinalFeedbackObj,
+    };
+
+    try {
+      await addNewEntryToDBService(payload);
+      toast({
+        title: "Interview saved!",
+        description:
+          "We have successfully saved this mock interview, you can check it anytime. ",
+      });
+    } catch (error) {
+      toast({
+        title: "Something went wrong while adding new AI Entry",
+        variant: "destructive",
+      });
+    }
+  };
+
+  useEffect(() => {
+    addNewEntryToDB();
+  }, []);
+
   return (
     <div className="flex flex-col gap-2">
       {Object.values(userFinalFeedbackObj).map((item: any, index) => {
