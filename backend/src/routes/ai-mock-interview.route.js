44be7e313170ed346710ab7cbcabbf0291cbf038
Samyak Shah <samyakshah3008@gmail.com>
2024-12-18 15:35:24 +0530

    feat - add new route with dynamic id

--- backend/src/routes/ai-mock-interview.route.js
+++ backend/src/routes/ai-mock-interview.route.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import {
   addNewAIMockInterview,
   deleteAIMockInterview,
+  getParticularAIMockInterviewData,
   getUserAIMockInterviewsData,
 } from "../controllers/ai-mock-interview.controller.js";
 import { verifyJWT } from "../middleware/auth.js";
@@ -9,6 +10,7 @@ import { verifyJWT } from "../middleware/auth.js";
 const router = Router();
 
 router.get("/", verifyJWT, getUserAIMockInterviewsData);
+router.get("/:id", verifyJWT, getParticularAIMockInterviewData);
 router.post("/", verifyJWT, addNewAIMockInterview);
 router.delete("/", verifyJWT, deleteAIMockInterview);
 
