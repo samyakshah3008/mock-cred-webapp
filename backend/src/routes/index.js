aea9e8b26034d4399be95ebe4139833e3e6b6a93
Samyak Shah <samyakshah3008@gmail.com>
2024-12-19 17:25:16 +0530

    feat - services route add

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import aiMockInterviewRouter from "./ai-mock-interview.route.js";
 import authRouter from "./auth.route.js";
 import availabilityRouter from "./availability.route.js";
-import eventsRouter from "./event.route.js";
+import servicesRouter from "./my-services.route.js";
 import notesRouter from "./note.route.js";
 import userRouter from "./user.route.js";
 
@@ -12,7 +12,7 @@ router.use("/auth", authRouter);
 router.use("/user", userRouter);
 router.use("/notes", notesRouter);
 router.use("/availability", availabilityRouter);
-router.use("/events", eventsRouter);
 router.use("/ai-mock-interview", aiMockInterviewRouter);
+router.use("/services", servicesRouter);
 
 export default router;
