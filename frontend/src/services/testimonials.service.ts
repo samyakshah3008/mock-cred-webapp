6af54e18593edaef6ef49a05664fbdf1ce6114b1
Samyak Shah <samyakshah3008@gmail.com>
2025-01-27 22:35:31 +0530

    feat: service functions for testimonials

--- frontend/src/services/testimonials.service.ts
+++ frontend/src/services/testimonials.service.ts
@@ -1,9 +1,48 @@
-import { get } from "@/config/API";
-import { testimonialEndpoint } from "@/constants/APIEndpoints";
+import { deleteRequest, get, postWithToken } from "@/config/API";
+import {
+  editTestimonialVisibilityEndpoint,
+  testimonialEndpoint,
+  updateTestimonialEndpoint,
+} from "@/constants/APIEndpoints";
 
 const fetchTestimonialsService = async (role: string) => {
   const response = get(`${testimonialEndpoint}?role=${role}`);
   return response;
 };
 
-export { fetchTestimonialsService };
+const editVisibilityService = async (
+  testimonialId: string,
+  currentRole: string,
+  showOnProfile: boolean
+) => {
+  const response = await postWithToken(editTestimonialVisibilityEndpoint, {
+    testimonialId,
+    role: currentRole,
+    showOnProfile,
+  });
+  return response;
+};
+
+const updateTestimonialService = async (testimonialData: any) => {
+  const response = await postWithToken(updateTestimonialEndpoint, {
+    ...testimonialData,
+  });
+  return response;
+};
+
+const deleteTestimonialService = async (
+  testimonialId: string,
+  testimonialReceiverUserId: string
+) => {
+  const response = await deleteRequest(
+    `${testimonialEndpoint}?testimonialId=${testimonialId}&testimonialReceiverUserId=${testimonialReceiverUserId}`
+  );
+  return response;
+};
+
+export {
+  deleteTestimonialService,
+  editVisibilityService,
+  fetchTestimonialsService,
+  updateTestimonialService,
+};
