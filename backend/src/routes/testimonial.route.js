b5ef69f58ef988a8931d67abbcd922ee04fb9b08
Samyak Shah <samyakshah3008@gmail.com>
2025-01-27 22:31:49 +0530

    feat: update, delete routes add

--- backend/src/routes/testimonial.route.js
+++ backend/src/routes/testimonial.route.js
@@ -1,9 +1,17 @@
 import { Router } from "express";
-import { getAllTestimonials } from "../controllers/testimonial.controller.js";
+import {
+  changeTestimonialVisibility,
+  deleteGivenTestimonial,
+  getAllTestimonials,
+  updateGivenTestimonial,
+} from "../controllers/testimonial.controller.js";
 import { verifyJWT } from "../middleware/auth.js";
 
 const router = Router();
 
 router.get("/", verifyJWT, getAllTestimonials);
+router.post("/edit-visibility", verifyJWT, changeTestimonialVisibility);
+router.post("/update", verifyJWT, updateGivenTestimonial);
+router.delete("/", verifyJWT, deleteGivenTestimonial);
 
 export default router;
