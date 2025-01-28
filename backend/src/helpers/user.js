8a8482effab228308f0620d8498977f142fedab5
Samyak Shah <samyakshah3008@gmail.com>
2025-01-28 11:42:39 +0530

    feat: helper functions add for user

---
+++ backend/src/helpers/user.js
@@ -0,0 +1,22 @@
+import { User } from "../models/user.model.js";
+
+const isEmailExists = async (email, userId) => {
+  const findEmail = await User.findOne({ email, _id: { $ne: userId } });
+  if (findEmail) {
+    return true;
+  }
+  return false;
+};
+
+const isUsernameExists = async (username, userId) => {
+  const findUsername = await User.findOne({
+    "onboardingDetails.stepOne.username": username,
+    _id: { $ne: userId },
+  });
+  if (findUsername) {
+    return true;
+  }
+  return false;
+};
+
+export { isEmailExists, isUsernameExists };
